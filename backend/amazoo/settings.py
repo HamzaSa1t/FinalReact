52a2582b21143ebd68681da93fa388dd21041bac
HamzaSait <37mezo@gmail.com>
2025-05-21 07:51:04 +0300

    allows cors

--- backend/amazoo/settings.py
+++ backend/amazoo/settings.py
@@ -191,7 +191,6 @@ CORS_ALLOWED_ORIGINS = [
  # "http://localhost:5175",
 ]
 
-CORS_ALLOW_CREDENTIALS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Replace with your SMTP server
 EMAIL_PORT = 587
